18a332974d8c3b019eabcfc00029b313e86511af
Emilio Florez <efc201417@gmail.com>
2025-08-29 13:52:57 -0500

    Fix: carousel bug

--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -1,37 +1,97 @@
-import React from 'react'
-import Slider from 'react-slick'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useState } from "react"
+import "./style.css"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+type Props = { images: string[]; };
 
-  type Props = {
-  images: string[];
-};
+export default function Slides({ images }: Props) {
+  const [currentSlide, setCurrentSlide] = React.useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
 
-function Slides({ images }: Props) {
   return (
-    <div className='w-70 md:w-120 lg:w-4/4 mx-auto'>
-        <Slider {...settings}>
-            {images.map((image, i) => (
-                console.log(image),
-                <div key={i} className="w-full">
-                <img
-                    src={image}
-                    alt={`slide-${i}`}
-                    className="w-full h-auto object-cover rounded-lg"
-                />
-                </div>
-            ))}
-        </Slider>
-    </div>
+    <>
+      <div className="navigation-wrapper pt-4">
+        <div ref={sliderRef} className="keen-slider">
+          {images.map((image, i) => (
+            <div key={i} className="keen-slider__slide number-slide1">
+              <img src={image} alt={`Slide ${i + 1}`} className="object-contain h-64 w-full rounded-xl" />
+            </div>
+          ))}
+        </div>
+        {loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
+
+            <Arrow
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            />
+          </>
+        )}
+      </div>
+      {loaded && instanceRef.current && (
+        <div className="dots">
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx)
+                }}
+                className={"dot" + (currentSlide === idx ? " active" : "")}
+              ></button>
+            )
+          })}
+        </div>
+      )}
+    </>
   )
 }
 
-export default Slides
+function Arrow(props: {
+  disabled: boolean
+  left?: boolean
+  onClick: (e: any) => void
+}) {
+  const disabled = props.disabled ? " arrow--disabled" : ""
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? "arrow--left" : "arrow--right"
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  )
+}
